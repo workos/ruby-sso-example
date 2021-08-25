d321e860068de0e0aa460b0eb8a87fca84cd8179
Blair Lunceford <blair@workos.com>
2021-08-25 12:15:57 -0600

    Update Ruby SSO example app

--- app.rb
+++ app.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
 require 'sinatra'
 require 'workos'
 require 'json'
 
-# Get your project_id and configure your domain and
-# redirect_uris at https://dashboard.workos.com/sso/configuration
-DOMAIN = 'acme.com'
-PROJECT_ID = 'project_01DG5TGK363GRVXP3ZS40WNGEZ'
+# Pull API key and Client ID from ENV variables
+WorkOS.key = ENV['WORKOS_API_KEY']
+CLIENT_ID = ENV['WORKOS_CLIENT_ID']
+
+# Configure your domain and redirect_uris at
+# https://dashboard.workos.com/configuration
+DOMAIN = 'foo-corp.com'
 REDIRECT_URI = 'http://localhost:4567/callback'
 
 use(
-  Rack::Session::Cookie, 
+  Rack::Session::Cookie,
   key: 'rack.session',
   domain: 'localhost',
   path: '/',
@@ -25,27 +29,35 @@ get '/' do
   erb :index, :layout => :layout
 end
 
+# Authenticate a user by sending them to the WorkOS API
+# You can also use connection or provider parameters
+# in place of the domain parameter
+# https://workos.com/docs/reference/sso/authorize/get
 get '/auth' do
   authorization_url = WorkOS::SSO.authorization_url(
     domain: DOMAIN,
-    project_id: PROJECT_ID,
+    client_id: CLIENT_ID,
     redirect_uri: REDIRECT_URI,
   )
 
   redirect authorization_url
 end
 
+# Exchange a code for a user profile at the callback route
 get '/callback' do
-  profile = WorkOS::SSO.profile(
+  profile_and_token = WorkOS::SSO.profile_and_token(
     code: params['code'],
-    project_id: PROJECT_ID,
+    client_id: CLIENT_ID,
   )
+
+  profile = profile_and_token.profile
 
   session[:user] = profile.to_json
 
   redirect '/'
 end
 
+# Logout a user
 get '/logout' do
   session[:user] = nil
 
